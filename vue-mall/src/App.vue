1432cb54e8da84ef60e9f3895ae7d1ce087275b0
letian-ready <ready_for_love@163.com>
2020-12-13 16:58:59 +0800

    product init

--- vue-mall/src/App.vue
+++ vue-mall/src/App.vue
@@ -19,22 +19,10 @@ export default {
     }
   },
   mounted(){
-    if(this.$cookie.get('userId')){
-      this.getUser();
-      this.getCartCount();
-    }
+    
   },
   methods:{
-    getUser(){
-      this.axios.get('/user').then((res={})=>{
-        this.$store.dispatch('saveUserName',res.username);
-      })
-    },
-    getCartCount(){
-      this.axios.get('/carts/products/sum').then((res=0)=>{
-        this.$store.dispatch('saveCartCount',res);
-      })
-    }
+    
   }
 }
 </script>
